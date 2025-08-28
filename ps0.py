793e23749a620ca9a09c4ccf92e3d0020417d2cf
isabellajohnson2413 <isabella.johnson2413@gmail.com>
2025-08-28 14:06:13 -0400

    I added a slide and screenshot, updating ps0.py

--- ps0.py
+++ ps0.py
@@ -1,2 +1,11 @@
-print('Hello world')
-
+Python 3.13.7 (v3.13.7:bcee1c32211, Aug 14 2025, 19:10:51) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: /Users/isabellajohnson/Documents/GitHub/ps-0-isabellajohnson2413/ps0.py
+Hello world
+>>> print("Hello, Boston College!")
+Hello, Boston College!
+>>> 7+15
+22
+>>> print("Goodbye Summer")
+Goodbye Summer
